115cedcf673ea16ea980f6d92dce821f38f5476b
Michiel Boekhoff <michielalexanderb@gmail.com>
2020-01-26 22:56:38 +0000

    Leverage the ApiClient in the RoundUpService.
    This is using the previously built integrations in the RoundUpService.

--- src/main/java/com/michielboekhoff/starlingtest/service/RoundupService.java
+++ src/main/java/com/michielboekhoff/starlingtest/service/RoundupService.java
@@ -1,13 +1,38 @@
 package com.michielboekhoff.starlingtest.service;
 
+import com.michielboekhoff.starlingtest.client.ApiClient;
+import com.michielboekhoff.starlingtest.client.Interval;
+import com.michielboekhoff.starlingtest.domain.Account;
 import com.michielboekhoff.starlingtest.domain.Transaction;
+import com.michielboekhoff.starlingtest.domain.Transaction.TransactionDirection;
 
 import java.math.BigDecimal;
+import java.time.Clock;
 import java.util.List;
 
 public class RoundupService {
-    public BigDecimal roundUp(List<Transaction> transactions) {
-        return transactions.stream()
+
+    private static final Interval LAST_WEEK_INTERVAL = Interval.lastWeek(Clock.systemUTC());
+
+    private final ApiClient apiClient;
+
+    public RoundupService(ApiClient apiClient) {
+        this.apiClient = apiClient;
+    }
+
+    public void roundUpTransactionsFromLastWeekIntoSavingsGoal(String savingsGoalUid) {
+        List<Account> accounts = apiClient.getAllAccounts();
+
+        for (Account account : accounts) {
+            BigDecimal totalToSave = getAmountToSaveForAccount(account);
+            apiClient.transferIntoSavingsGoalForAccount(account, savingsGoalUid, totalToSave);
+        }
+    }
+
+    private BigDecimal getAmountToSaveForAccount(Account account) {
+        return apiClient.getAllTransactionsForAccountAndDefaultCategoryInInterval(account, LAST_WEEK_INTERVAL)
+                .stream()
+                .filter(transaction -> TransactionDirection.OUT.equals(transaction.getTransactionDirection()))
                 .map(Transaction::getAmount)
                 .map(this::getAmountToSave)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
--- src/test/java/com/michielboekhoff/starlingtest/service/RoundupServiceTest.java
+++ src/test/java/com/michielboekhoff/starlingtest/service/RoundupServiceTest.java
@@ -1,5 +1,7 @@
 package com.michielboekhoff.starlingtest.service;
 
+import com.michielboekhoff.starlingtest.client.ApiClient;
+import com.michielboekhoff.starlingtest.domain.Account;
 import com.michielboekhoff.starlingtest.domain.Transaction;
 import com.michielboekhoff.starlingtest.domain.Transaction.TransactionDirection;
 import org.junit.jupiter.api.DisplayName;
@@ -8,31 +10,51 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import java.util.List;
 
-import static java.util.Collections.emptyList;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 class RoundupServiceTest {
 
-    private final RoundupService roundupService = new RoundupService();
+    private static final String SAVINGS_GOAL_UID = "savingsGoalUid";
+
+    private static final Account FIRST_ACCOUNT = new Account("accountOne", "defaultCategory");
+    private static final Account SECOND_ACCOUNT = new Account("accountTwo", "defaultCategory");
+
+    private final ApiClient apiClient = mock(ApiClient.class);
+    private final RoundupService roundupService = new RoundupService(apiClient);
+
+    private final List<Transaction> transactions = List.of(
+            new Transaction(new BigDecimal("4.35"), TransactionDirection.OUT),
+            new Transaction(new BigDecimal("5.20"), TransactionDirection.OUT),
+            new Transaction(new BigDecimal("0.87"), TransactionDirection.OUT)
+    );
 
     @Test
-    @DisplayName("roundUp should calculate the amount to be saved")
-    void roundUpShouldCalculateSavings() {
-        List<Transaction> transactions = List.of(
-                new Transaction(new BigDecimal("1.88"), TransactionDirection.OUT),
-                new Transaction(new BigDecimal("5.27"), TransactionDirection.OUT)
-        );
+    @DisplayName("it should round up all transactions into a savings goal for an account")
+    void shouldRoundUpTransactionsForAnAccount() {
+        when(apiClient.getAllAccounts()).thenReturn(List.of(FIRST_ACCOUNT));
+        when(apiClient.getAllTransactionsForAccountAndDefaultCategoryInInterval(eq(FIRST_ACCOUNT), any()))
+                .thenReturn(transactions);
 
-        BigDecimal savingAmount = roundupService.roundUp(transactions);
+        roundupService.roundUpTransactionsFromLastWeekIntoSavingsGoal(SAVINGS_GOAL_UID);
 
-        assertThat(savingAmount).isEqualTo(new BigDecimal("0.85"));
+        verify(apiClient)
+                .transferIntoSavingsGoalForAccount(any(), eq(SAVINGS_GOAL_UID), eq(new BigDecimal("1.58")));
     }
 
     @Test
-    @DisplayName("roundUp should return zero if there are no transactions")
-    void roundUpWithoutTransactions() {
-        BigDecimal savingAmount = roundupService.roundUp(emptyList());
+    @DisplayName("it should round up the transactions into a savings goal per account")
+    void shouldRoundUpTransactionsPerAccount() {
+        when(apiClient.getAllAccounts()).thenReturn(List.of(FIRST_ACCOUNT, SECOND_ACCOUNT));
+        when(apiClient.getAllTransactionsForAccountAndDefaultCategoryInInterval(eq(FIRST_ACCOUNT), any()))
+                .thenReturn(List.of(new Transaction(new BigDecimal("1.58"), TransactionDirection.OUT)));
+        when(apiClient.getAllTransactionsForAccountAndDefaultCategoryInInterval(eq(SECOND_ACCOUNT), any()))
+                .thenReturn(List.of(new Transaction(new BigDecimal("0.84"), TransactionDirection.OUT)));
 
-        assertThat(savingAmount).isEqualTo(BigDecimal.ZERO);
+        roundupService.roundUpTransactionsFromLastWeekIntoSavingsGoal(SAVINGS_GOAL_UID);
+
+        verify(apiClient).transferIntoSavingsGoalForAccount(FIRST_ACCOUNT, SAVINGS_GOAL_UID, new BigDecimal("0.42"));
+        verify(apiClient).transferIntoSavingsGoalForAccount(SECOND_ACCOUNT, SAVINGS_GOAL_UID, new BigDecimal("0.16"));
     }
 }
